56f8c93a06ba0138478b4678d5543a56ae563487
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-02-17 13:05:17 +0100

    added functions

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 // declaring variables 
 var firstCard = 7
 var secondCard = 10
+var cards = [firstCard, secondCard]
 var sum = firstCard + secondCard
 var hasBlackJack = false
 var isAlive = true
 var gameMessage = ""
 var getMessage = document.getElementById("get-message")
+var getSum = document.getElementById("get-sum")
+var getCards = document.getElementById("get-cards")
 
+function startGame() {
+    renderGame()
+}
 
-function startgame() {
+function renderGame() {
+    getCards.textContent = "Cards: " + cards[0] + " " + cards[1]
+    getSum.textContent = "Sum: " + sum
     if (sum <= 21) {
         gameMessage = "Do you want to draw a new card?!"
     } else if (sum === 21) {
-        gameMessage = "Nicee! You've got Blackjack"
+        gameMessage = "Nicee!You've got Blackjack"
     } else {
-        gameMessage = "You lost!"
+        gameMessage = "You're out of the game!"
         isAlive = false
     }    
     getMessage.textContent = gameMessage
+}
+
+function newCard() {
+    anotherCard = 5
+    sum += anotherCard
+    renderGame()
 }
